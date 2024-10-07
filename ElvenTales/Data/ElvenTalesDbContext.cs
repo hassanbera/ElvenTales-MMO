789e66f5c84d972327a872a5715431298bce3b6f
Hasan Bera Kabadayı <hasanbera0619@gmail.com>
2024-10-07 17:26:50 +0300

    Initial commit for ElvenTales-MMO

---
+++ ElvenTales/Data/ElvenTalesDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ElvenTales.Models;
+using System.Collections.Generic;
+
+namespace ElvenTales.Data
+
+
+{
+    public class ElvenTalesDbContext : DbContext
+    {
+        public ElvenTalesDbContext(DbContextOptions<ElvenTalesDbContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
